df2ea694deabb7589c2c00ebd76aed7d8687f575
Ivandro Jao <ivandrofly@gmail.com>
2024-05-09 16:08:22 +0100

    Add AutoMapperProfile for TodoItem mapping
    A new AutoMapperProfile has been added specifically for mapping the TodoItem entity to TodoItemDto. This profile greatly improves the ease and efficiency of data transformation between the TodoItem object and its corresponding data transfer object (Dto).

---
+++ src/Application/Common/Mappings/Profiles/AutoMapperProfile.cs
@@ -0,0 +1,12 @@
+﻿using Assignment.Application.TodoLists.Queries.GetTodos;
+using Assignment.Domain.Entities;
+
+namespace Assignment.Application.Common.Mappings.Profiles;
+
+public class AutoMapperProfile : Profile
+{
+    public AutoMapperProfile()
+    {
+        CreateMap<TodoItem, TodoItemDto>();
+    }   
+}
