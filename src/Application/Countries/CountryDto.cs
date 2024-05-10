282f7c7b95957735d81978c58f9e21fe10d7d68a
Ivandro Jao <ivandrofly@gmail.com>
2024-05-10 14:52:32 +0100

    Add Country and City entities to application context
    A new feature has been introduced for managing Country and City entities in the Application context. The IApplicationDbContext interface has been modified to include DbSet for Country and City. Also, a Query handler for Country and DTOs for Country and City have been implemented.

---
+++ src/Application/Countries/CountryDto.cs
@@ -0,0 +1,16 @@
+﻿using Assignment.Domain.Entities;
+
+namespace Microsoft.Extensions.DependencyInjection.Countries;
+
+public class CountryDto
+{
+    public string? Name { get; set; }
+    
+    private class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<Country, CountryDto>();
+        }
+    }
+}
---
+++ src/Application/Countries/CountryQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using Assignment.Application.Common.Interfaces;
+using Assignment.Application.Common.Security;
+using Assignment.Application.TodoLists.Queries.GetTodos;
+using Assignment.Domain.Entities;
+
+namespace Microsoft.Extensions.DependencyInjection.Countries;
+
+[Authorize]
+public record CountryQuery : IRequest<IList<CountryDto>>;
+
+public class CountryQueryHandler : IRequestHandler<CountryQuery, IList<CountryDto>>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public CountryQueryHandler(IApplicationDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<IList<CountryDto>> Handle(CountryQuery request, CancellationToken cancellationToken)
+    {
+        return await _context.Countries
+            .AsNoTracking()
+            .ProjectTo<CountryDto>(_mapper.ConfigurationProvider)
+            .OrderBy(t => t.Name)
+            .ToListAsync(cancellationToken);
+    }
+}
