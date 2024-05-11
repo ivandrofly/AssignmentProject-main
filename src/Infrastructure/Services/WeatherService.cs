151a24e211fe896aff8c37d5ba05c05a9690e62d
Ivandro Jao <ivandrofly@gmail.com>
2024-05-11 18:27:40 +0100

    Update WeatherService and related unit tests
    The WeatherService.cs file has been updated with more detailed comments, better clarifying its functionality. UnitTest1.cs files have been deleted and replaced with WeatherServiceTest.cs to better match the underlying service being tested. The NSubstitute package has been added for more robust testing abilities.

--- src/Infrastructure/Services/WeatherService.cs
+++ src/Infrastructure/Services/WeatherService.cs
@@ -2,8 +2,17 @@
 
 namespace Microsoft.Extensions.DependencyInjection.Services;
 
+/// <summary>
+/// Represents a weather service that provides weather forecast data.
+/// </summary>
 public class WeatherService : IWeatherForecastApi
 {
+    /// <summary>
+    /// Retrieves the temperature for a given city at a specific time.
+    /// </summary>
+    /// <param name="cityName">The name of the city.</param>
+    /// <param name="time">The specific time for which to retrieve the temperature.</param>
+    /// <returns>The temperature of the given city at the specified time.</returns>
     public int GetTemperature(string cityName, DateTime time)
     {
         return Random.Shared.Next(-10, 60);
---
+++ src/Tests/Infrastructure/Services/WeatherServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection.Services;
+
+namespace Tests.Infrastructure.Services;
+
+public class WeatherServiceTest
+{
+    [Fact]
+    public void GetTemperatureTest()
+    {
+        // Arrange
+        var sut = new WeatherService();
+
+        // Act
+        var ouptut = sut.GetTemperature("Portugal", DateTime.Now);
+
+        // Assert
+        Assert.InRange(ouptut, -10, 60);
+    }
+}
